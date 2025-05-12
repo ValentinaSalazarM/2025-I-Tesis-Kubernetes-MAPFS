6f1291d83456f280a29dd2e5c10157a423a12cf2
Valentina Salazar Marín <50112268+ValentinaSalazarM@users.noreply.github.com>
2025-05-12 18:40:47 -0500

    Correciones en la documentación

--- deploy.ps1
+++ deploy.ps1
@@ -5,12 +5,16 @@ docker build -t mapfs-cloud:$tag -f ./IoT/cloud/MAPFS/Dockerfile ./IoT
 docker build -t mapfs-gateway:$tag -f ./IoT/gateway/MAPFS/Dockerfile ./IoT
 docker build -t mapfs-device:$tag -f ./IoT/device/MAPFS/Dockerfile ./IoT
 docker build -t mapfs-locust-image:$tag -f ./attacks/Dockerfile.locust ./attacks
+docker build -t sniffer-capture:latest -f attacks/Dockerfile.capture ./attacks
+docker build -t sniffer-replicate:latest -f attacks/Dockerfile.replicate ./attacks
 
 Write-Host "Cargando imágenes en Minikube"
 minikube image load mapfs-cloud:$tag
 minikube image load mapfs-gateway:$tag
 minikube image load mapfs-device:$tag
 minikube image load mapfs-locust-image:$tag
+minikube image load sniffer-capture:latest
+minikube image load sniffer-replicate:latest
 
 Write-Host "Aplicando manifiestos con Kustomize"
 kubectl apply -k .
